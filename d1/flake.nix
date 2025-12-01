746250f53a2da9ecb50a2f967280fdc891da6fb9
silent-brad <bradscottwhite@gmail.com>
2025-12-01 10:16:06 -0500

    got setup working

---
+++ d1/flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Fennel Dev Environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        my-buildInputs = with pkgs; [
+          luajit
+          luajitPackages.fennel
+          fennel-ls
+          fnlfmt
+        ];
+      in {
+        packages.default = pkgs.writeShellScriptBin "run" ''
+          ${pkgs.luajitPackages.fennel}/bin/fennel -c main.fnl > main.lua
+          #LUA_PATH="?.lua;"
+          ${pkgs.luajit}/bin/luajit main.lua
+        '';
+        devShells.default = pkgs.mkShell { buildInputs = my-buildInputs; };
+      });
+}
